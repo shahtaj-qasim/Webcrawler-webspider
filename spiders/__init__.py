da0e43ca394957edfb975f5c9573b16108d5844f
Shahtaj Qasim <61311949+shahtaj-qasim@users.noreply.github.com>
2020-06-27 09:36:39 +0200

    Update __init__.py

--- spiders/__init__.py
+++ spiders/__init__.py
@@ -2,3 +2,16 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+# Crawler Implementation
+Note: Implementation for crawler can be found in:
+1. gadetector/gadetector/spiders/GASpider.py
+2. gadetector/gadetector/items.py
+3. gadetector/gadetector/plotting.py
+I used Scrapy Framework for crawling in Python3. A project was created using command:
+        scrapy startproject gadetector
+After creating the project we need to generate a spider (named GASpider) using command:
+        scrapy genspider GASpider
+Results were generated in a file named garesults.csv. The
+command used for running the spider and generating results is below:
+        scrapy crawl -o garesults.csv GASpider
